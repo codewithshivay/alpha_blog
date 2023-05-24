f8ebefeabff68a3d3fc4e27d30030d276900c0dc
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-05-24 15:13:28 +0530

    add some constraint to article models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :title, presence: true, length: { in: 5..100 }, uniqueness: true
   validates :discription, presence: true, length: { minimum: 10, maximum: 200 }
   validates :testing, numericality: { even: true }
 end

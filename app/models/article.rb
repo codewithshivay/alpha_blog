57bdd8d2ad28fb0a10e6e3128761c17519aa62a2
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-05-25 12:19:18 +0530

    added some more validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { in: 5..100 }, uniqueness: true
-  validates :discription, presence: true, length: { minimum: 10, maximum: 200 }
-  validates :testing, numericality: { even: true }
+  validates :discription, presence: true, length: { minimum: 10, maximum: 200 }, format: { with: /\A[a-zA-Z ]+\z/, message: "allow only letters"}
+  validates :bar_code, numericality: { even: true, message: "%{value}valid only even numbers" }
 end

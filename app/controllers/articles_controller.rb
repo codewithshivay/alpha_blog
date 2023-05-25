68c689680804d1c848310a607047ee01007b1246
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-05-25 19:10:59 +0530

    added new and create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :discription, :bar_code))
+        @article.save
+        #render plain: @article.inspect -> for showing the value
+        #redirect_to article_path(@article) -> this will also work same
+        redirect_to @article
+    end
+
 end

0c403c9623ce2cddbedafb2d88dffe706ac215f4
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-09-21 16:03:07 +0530

    Add one to many association between users and articles from back end

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { in: 5..100 }, uniqueness: true
   validates :discription, presence: true, length: { minimum: 10, maximum: 200 }
   validates :bar_code, numericality: { even: true, message: "valid only even numbers" }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_20_133155) do
+ActiveRecord::Schema.define(version: 2023_09_21_101529) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2023_09_20_133155) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "bar_code"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

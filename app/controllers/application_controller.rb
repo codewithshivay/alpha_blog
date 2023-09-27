e886f74c1fae04c40d4923e62117ec4c5a165169
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-09-27 20:37:33 +0530

    controller as helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user, :Logged_in?
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def Logged_in?
+        !!current_user
+    end
+
 end

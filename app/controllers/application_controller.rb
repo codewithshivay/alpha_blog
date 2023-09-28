6003ded7c8f6624c6838f8d77343c6eddee624ef
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-09-28 17:12:11 +0530

    Added require user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
         !!current_user
     end
 
+    def require_user
+        if !Logged_in?
+            flash[:alert] = "You must logged in to perform that action"
+            redirect_to login_path
+        end
+    end
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_articel, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
         #@article = Article.find(params[:id])
@@ -55,6 +57,13 @@ class ArticlesController < ApplicationController
 
     def article_param
       params.require(:article).permit(:title, :discription, :bar_code)
+    end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
     end
 
 end

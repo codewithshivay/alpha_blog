67eeb64648ba4170b5aa60a5d238e6ed9f5946db
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-12-14 15:45:12 +0530

    adding controller level restriction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :set_articel, only: [:show, :edit, :update, :destroy]
-    before_action :require_user, except: [:show, :index]
+    before_action :require_user, except: [:show]
     before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show

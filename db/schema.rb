82d422dd874eadd973e853c4b863dad6ff1c809f
Shivam Mahajan <pro.dev.shivay@gmail.com>
2023-12-20 18:42:39 +0530

    Added CategoryTable to the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_04_085919) do
+ActiveRecord::Schema.define(version: 2023_12_20_130222) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
